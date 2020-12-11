259a7e452aa831b780cc9173104486c07abdbc78
Marcos Carvalho <marcos@mcexpert.com.br>
2020-12-11 20:13:26 -0300

    a

--- scrape-site/app.py
+++ scrape-site/app.py
@@ -4,12 +4,15 @@ import time
 
 driver = webdriver.Chrome(executable_path=os.getcwd() + os.sep + 'chromedriver')
 driver.get ('https://www.heise.de/download/search#?page=1&sort=DOWNLOADRANK')
+#driver.get ('https://www.globo.com/')
 
 time.sleep (4)
-botao_acept = driver.find_element_by_xpath('//button[@class="sc-bqGHjH qNjkR"]')
-botao_acept.click()
+botao_acept = driver.find_elements_by_xpath('//button[@data-testid="uc-accept-all-button"]')
+#botao_acept = driver.find_element_by_xpath('//a[@class="hui-premium__link"]')
+for bot in botao_acept:
+    bot.click()
 time.sleep (4)
-titulos2 = driver.find_element_by_xpath('//h3[@class="gamma ng-binding"]')
+titulos = driver.find_elements_by_xpath('//h3[@class="gamma ng-binding"]')
 
-for titulo in titulos2:
+for titulo in titulos:
     print (titulo.text)
